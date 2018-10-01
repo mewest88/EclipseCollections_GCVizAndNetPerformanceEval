0acedd44f15add293fb49496f63781e250b682a4
Zander Nickle <znickle24@icloud.com>
2018-10-01 13:18:42 -0600

    Updated the UserProgram and the UserProgramMBean interface.

--- TestData_GCInfo/TestFile_1/src/UserProgram.java
+++ TestData_GCInfo/TestFile_1/src/UserProgram.java
@@ -1,11 +1,22 @@
 public class UserProgram implements UserProgramMBean
 {
+  private long id1;
+  private long id2;
+  //username and password to be used if the connection is remote, local is optional for these values
+  private String userName;
+  private String password;
+
   @Override
   public void getUserInfo() {
-
+    System.out.println("This is the username: " + userName);
   }
   @Override
+  public void setUserInfo(String _userName) {
+    userName = _userName;
+  }
+  //don't allow them to set the user id. This should be found by grabbing the info from the connection
+  @Override
   public void getGCInstance() {
-    
+
   }
 }
--- TestData_GCInfo/TestFile_1/src/UserProgramMBean.java
+++ TestData_GCInfo/TestFile_1/src/UserProgramMBean.java
@@ -1,6 +1,7 @@
 public interface UserProgramMBean
 {
   public void getUserInfo();
+  public void setUserInfo(String _userName);
   public void getGCInstance();
   //add getters and setters to interface for reading and writing priviledges
 }
