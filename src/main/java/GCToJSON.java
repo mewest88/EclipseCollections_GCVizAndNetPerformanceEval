16730bd76fdf0b7fdb20a5d0959d0baabba951c2
Mason West <mew0830@gmail.com>
2018-10-02 15:07:10 -0600

    imported gson and got maven working

--- src/main/java/GCToJSON.java
+++ src/main/java/GCToJSON.java
@@ -1,10 +1,12 @@
+import com.google.gson.Gson;
+
 public class GCToJSON {
 
-//    String GCType;
-//    int GCTime; // In milliseconds
-//
-//    public Gson GCToJson(String gctype, int time) {
-//        GCType = gctype;
-//        GCTime = time;
-//    }
+    String GCType;
+    int GCTime; // In milliseconds
+
+    public void GCToJson(String gctype, int time) {
+        GCType = gctype;
+        GCTime = time;
+    }
 }
